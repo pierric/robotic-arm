549297404f101f246c0d27ba346099663f7c9181
Jiasen Wu <jiasenwu@hotmail.com>
2024-09-13 23:29:45 +0200

    dynamic service

---
+++ arm_dynamics/src/main.py
@@ -0,0 +1,96 @@
+import os
+from typing import List, Tuple, Optional
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import roboticstoolbox as rtb
+import spatialmath as sm
+import numpy as np
+
+import utils
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://moon:3000",
+    "http://moon.local:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def from_urdf(file_path, tld=None, xacro_tld=None):
+    links, name, urdf_string, urdf_filepath = rtb.Robot.URDF_read(
+        file_path, tld=tld, xacro_tld=xacro_tld
+    )
+
+    return rtb.Robot(
+        links,
+        name=name,
+        urdf_string=urdf_string,
+        urdf_filepath=urdf_filepath,
+    )
+
+
+class ShiftRequest(BaseModel):
+    q: List[float]
+    offset: Tuple[float, float, float]
+
+
+class ShiftResponse(BaseModel):
+    path: List[List[float]]
+    arrived: bool
+
+
+PACKAGE_DIR = os.path.split(__file__)[0]
+# sign because of the direction is different between the real
+# hardware and the urdf (TODO fixup the urdf)
+REDUCTION_RATIOS = [-6, -20, 20, -4, -4, -10]
+# FIXUP on axis-Z because of the difference of point 0
+FIXUP = [0, 0, 0.785, 0, 0, 0]
+
+
+@app.post("/plan")
+def read_item(shift: ShiftRequest):
+    parol6 = from_urdf("urdf/PAROL6.urdf.xacro", tld=PACKAGE_DIR, xacro_tld=PACKAGE_DIR)
+
+    path = []
+    arrived = False
+
+    q = [v / r + p for v, r, p in zip(shift.q, REDUCTION_RATIOS, FIXUP)]
+    Tep = parol6.fkine(q) * sm.SE3.Trans(*shift.offset)
+
+    while not arrived and len(path) < 10:
+        v, arrived = rtb.p_servo(parol6.fkine(q), Tep, 1)
+        qd = np.linalg.pinv(parol6.jacobe(q)) @ v
+        q = q + qd * 0.05
+        path.append(q)
+
+    def _revert(q):
+        return [(v - p) * r for v, r, p in zip(q, REDUCTION_RATIOS, FIXUP)]
+
+    path = [_revert(q) for q in path]
+
+    return ShiftResponse(path=path, arrived=arrived)
+
+
+class Step(BaseModel):
+    gripper: Optional[float] = None
+    positions: Optional[List[float]] = None
+
+
+class PlanRequest(BaseModel):
+    path: List[Step]
+
+
+@app.post("/execute")
+async def execute(plan: PlanRequest):
+    await utils.execute(plan.path)
---
+++ arm_dynamics/src/utils.py
@@ -0,0 +1,46 @@
+import os
+import json
+import asyncio
+
+import aiomqtt
+from websockets.asyncio.client import connect as ws_connect
+
+
+MQTT_BROKER_URL = os.environ["MQTT_BROKER_URL"]
+MQTT_BROKER_PORT = int(os.environ.get("MQTT_BROKER_PORT", 1883))
+MOONRAKER_URL = os.environ["MOONRAKER_URL"]
+
+
+async def run_gcode(ws, gcode):
+    await ws.send(
+        json.dumps({
+            "jsonrpc": "2.0",
+            "method": "printer.gcode.script",
+            "params": {
+                "script": gcode + "\nM400"
+            },
+            "id": 200
+        })
+    )
+
+    while True:
+        resp = await ws.recv()
+        resp = json.loads(resp)
+        if "result" in resp:
+            print(resp)
+            break
+
+
+async def execute(path):
+    async with aiomqtt.Client(MQTT_BROKER_URL, port=MQTT_BROKER_PORT) as mqtt:
+        async with ws_connect(MOONRAKER_URL) as ws:
+            for s in path:
+                if pos := s.positions:
+                    gcode = "G1 " + " ".join(
+                        [n+str(v) for n,v in zip("XYZABC", pos)]
+                    )
+                    await run_gcode(ws, gcode)
+
+                if gr := s.gripper:
+                    await mqtt.publish("/manipulator/command", gr)
+                    await asyncio.sleep(0.5)
